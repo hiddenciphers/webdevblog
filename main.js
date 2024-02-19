da0f64d5f216858e98b4ff8f77cedae0abced6d4
hiddenciphers <psolvyns@protonmail.com>
2024-02-19 14:52:06 +1100

    Adding contents dropdown menu

--- main.js
+++ main.js
@@ -51,6 +51,26 @@ document.addEventListener('DOMContentLoaded', function () {
     // Call the function to fetch and display all blog posts
     fetchAndDisplayPosts();
 
+    /* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
     // Toggle between dark and light modes
     const modeToggle = document.getElementById('mode-toggle');
     modeToggle.addEventListener('click', () => {
